86537fe516fc9b6a47919ae4998e30249c2428aa
RudraPratapRudra <75071229+RudraPratapRudra@users.noreply.github.com>
2021-10-08 13:26:55 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "BIG LIGHT";
+const accessCode2 = "DORACAKE";
+const accessCode3 = "NOBITA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("T G H I L G B I ", 100,50)
+    fill("black")
+    text("Hint: A GADGET THAT MAKE ANYONE BIGGER !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A K C E R A D O", 680,150)
+    fill("black")
+    text("Hint: DORAEMON's FAVOURITE FOOD !!", 680,170)
+
+    fill("white")
+    textSize(15)
+    text("I T A N B O", 100,250)
+    fill("black")
+    text("Hint: DORAEMON BEST FRIEND !!", 100,270)
+
+}
